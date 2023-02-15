1fd407f13df251c551359760eddce72b92eb0b25
CintyaAprilaFandini <cintyaacaf07@gmail.com>
2023-02-15 09:24:37 +0700

    5-Component

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
 const root = document.getElementById("root");
 
+function Link(props){
+          const link = document.createElement("a");
+          link.href = props.href;
+          link.textContent = props.label;
+          link.onclick = function(event) {
+                    event.preventDefault();
+                    const component = props.Component();
+                    root.innerHTML = "";
+                    root.append(component);
+                    history.pushState(null, "", event.target.href)
+          };
 
+          return link;
+}
 function Navbar(){
-          const linkHome = document.createElement("a");
-          linkHome.href = "#home";
-          linkHome.textContent = "Home";
-          linkHome.onclick = function(event) {
-                    event.preventDefault();
-                    const homeScreen = HomeScreen();
-                    root.innerHTML = "";
-                    root.append(homeScreen);
-                    history.pushState(null, "", event.target.href)
-          };
-
-          const linkAbout = document.createElement("a");
-          linkAbout.href = "#about";
-          linkAbout.textContent = "About";
-          linkAbout.onclick = function (event){
-                    event.preventDefault();
-                    const aboutScreen = AboutScreen();
-                    root.innerHTML = "";
-                    root.append(aboutScreen);
-                    history.pushState(null, "", event.target.href)
-          
-          };
+          const linkHome = Link({
+                    href: "#Home",
+                    label: "Home", 
+                    Component: HomeScreen,
+          });
+ 
+          const linkAbout = Link({
+                    href: "#About",
+                    label: "About", 
+                    Component: AboutScreen,
+          })
 
           const div = document.createElement("div");
           div.append(linkHome);
@@ -33,16 +35,11 @@ function Navbar(){
 }
 
 function AboutScreen(){
-          const linkHome = document.createElement("a");
-          linkHome.href = "#home";
-          linkHome.textContent = "Kembali Ke Home";
-          linkHome.onclick = function(event) {
-                    event.preventDefault();
-                    const homeScreen = HomeScreen();
-                    root.innerHTML = "";
-                    root.append(homeScreen);
-                    history.pushState(null, "", event.target.href)
-          };
+          const linkHome = Link({
+                    href: "#Home",
+                    label: "Kembali Ke Home", 
+                    Component: HomeScreen,
+          });
 
           const text = document.createElement("p");
           text.textContent = "welcome to About";
@@ -57,14 +54,11 @@ function AboutScreen(){
 function HomeScreen(){
           const navbar = Navbar();
           const textPreview = document.createElement("p");
- 
-
           const input = document.createElement("input");
           input.oninput = function(event){
                     textPreview.textContent = event.target.value;
           }
           input.placeholder = "Enter your name";
-
           const div = document.createElement("div");
           div.append(navbar);
           div.append(input);
